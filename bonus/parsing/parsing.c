d103b31bbed3559fd114f64b7a6006914be6d41c
rms35 <martinsoeder35@gmail.com>
2026-03-27 18:34:31 +0100

    fix: correct error message length in parsing.c

--- bonus/parsing/parsing.c
+++ bonus/parsing/parsing.c
@@ -73,7 +73,7 @@ t_map	*parsing(const int argc, const char *argv)
 		return (write(2, "Error: malloc\n", 15), NULL);
 	len = ft_strlen(argv);
 	if (ft_strncmp(&argv[len - 4], ".cub", 4))
-		return (write(2, "Error: invalid map file extension\n", 30), free(map),
+		return (write(2, "Error: invalid map file extension\n", 35), free(map),
 			NULL);
 	if (!get_width_height(argv, map))
 		return (free(map), NULL);
