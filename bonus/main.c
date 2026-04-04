3b9f6876fff3de21b95b1c0ffa8da4add69f9e32
rms35 <martinsoeder35@gmail.com>
2026-04-04 19:07:53 +0200

    refactor(parsing): update player and resource initialization

--- bonus/main.c
+++ bonus/main.c
@@ -6,25 +6,13 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 12:00:00 by rafael            #+#    #+#             */
-/*   Updated: 2026/03/06 18:30:00 by rafael           ###   ########.fr       */
+/*   Updated: 2026/04/02 14:30:00 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d_bonus.h"
 
 #define P1_FOV 1.7
-
-void print_grid(const char *grid, size_t width, size_t height)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < width * height)
-	{
-		write(1, grid + i, width);
-		i += width;
-	}
-}
 
 static int	game_loop(t_win *win)
 {
@@ -42,21 +30,15 @@ static int	game_loop(t_win *win)
 	if (win->player->m_delta_y != 0)
 	{
 		win->player->pitch -= win->player->m_delta_y;
+		if (win->player->pitch > 400)
+			win->player->pitch = 400;
+		if (win->player->pitch < -400)
+			win->player->pitch = -400;
 		win->player->m_delta_y = 0;
-		if (win->player->pitch > HEIGHT / 2)
-			win->player->pitch = HEIGHT / 2;
-		if (win->player->pitch < -HEIGHT / 2)
-			win->player->pitch = -HEIGHT / 2;
 	}
 	render_frame(win);
-	animate_sprites(&win->sprites[0], now * 1000);
-	mlx_put_image_to_window(win->mlxptr, win->winptr, win->img->img, 0, 0);
-	now = get_time();
-	if (now - win->last_frame < (double)FRAME_DELAY / 1000.0)
-		usleep(((double)FRAME_DELAY / 1000.0 - (now - win->last_frame)) * 1e6);
 	return (0);
 }
-
 
 int	main(int argc, char **argv)
 {
@@ -69,26 +51,21 @@ int	main(int argc, char **argv)
 	map = parsing(argc, argv[1]);
 	if (!map)
 		return (1);
-	print_grid(map->grid, map->width, map->height);
-	free(map->grid);
-	free(map);
-	return (0);
 	ft_bzero(&win, sizeof(t_win));
 	win.z_buffer = ft_calloc(WIDTH, sizeof(double));
 	if (!win.z_buffer)
 	{
 		write(2, "Error: malloc\n", 8);
-		exit(EXIT_FAILURE);
+		return (1);
 	}
-	win.img = &img;
 	win.keys = keys;
 	win.player = &player;
-	setup_mlx(&win, win.img);
-	map = get_mock_map(&win);
-	init_player(&player, map, P1_FOV);
-	init_sprites(&win, map);
 	win.map = map;
-	win.last_frame = get_time();
+	win.img = &img;
+	init_player(&player, map, P1_FOV);
+	setup_mlx(&win, &img);
+	if (init_textures(&win) || init_sprites(&win, map))
+		return (1);
 	mlx_loop_hook(win.mlxptr, game_loop, &win);
 	mlx_loop(win.mlxptr);
 	return (0);
--- bonus/parsing/init.c
+++ bonus/parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 17:00:00 by rafael            #+#    #+#             */
-/*   Updated: 2026/03/06 17:35:45 by rafael-m         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:45:00 by rafael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,142 +15,155 @@
 # define P1_TURNSP 0.013
 # define P1_R 0.2
 
-static void	get_dir(t_player *p, const char c)
+static void    get_dir(t_player *p, const char c)
 {
-	if (c == 'N')
-	{
-		p->dir_x = 0;
-		p->dir_y = -1;
-	}
-	if (c == 'S')
-	{
-		p->dir_x = 0;
-		p->dir_y = 1;
-	}
-	if (c == 'W')
-	{
-		p->dir_x = 1;
-		p->dir_y = 0;
-	}
-	if (c == 'E')
-	{
-		p->dir_x = -1;
-		p->dir_y = 0;
-	}
+       if (c == 'N')
+       {
+               p->dir_x = 0;
+               p->dir_y = -1;
+       }
+       if (c == 'S')
+       {
+               p->dir_x = 0;
+               p->dir_y = 1;
+       }
+       if (c == 'W')
+       {
+               p->dir_x = 1;
+               p->dir_y = 0;
+       }
+       if (c == 'E')
+       {
+               p->dir_x = -1;
+               p->dir_y = 0;
+       }
 }
 
-// TODO: Proper error handling (maybe OK now)
-void	init_player(t_player *p, const t_map *map, const
-	double fov)
+void   init_player(t_player *p, const t_map *map, const double fov)
 {
-	size_t				i;
+       size_t                          i;
 
-	ft_bzero(p, sizeof(t_player));
-	i = 0;
-	while (i < map->width * map->height && !ft_strchr(PLYR_DIR, map->grid[i]))
-		i++;
-	if (i >= map->width * map->height)
-	{
-		write(2, "Error: No player found in map\n", 30);
-		exit(1);
-	}
-	p->pos_x = (double)(i % map->width) + 0.5;
-	p->pos_y = (double)(i / map->width) + 0.5;
-	get_dir(p, map->grid[i]);
-	map->grid[i] = '0';
-	p->camp_x = -p->dir_y * (tan(fov / 2));
-	p->camp_y = p->dir_x * (tan(fov / 2));
-	p->speed = P1_SPEED;
-	p->rot_speed = P1_TURNSP;
-	p->cos_r = cos(p->rot_speed);
-	p->sin_r = sin(p->rot_speed);
-	p->cos_l = cos(-p->rot_speed);
-	p->sin_l = sin(-p->rot_speed);
-	p->fov = fov;
-	p->dir_mag = sqrt(p->dir_x * p->dir_x + p->dir_y * p->dir_y);
-	p->radius = P1_R;
-	p->fwd_speed = P1_SPEED;
-	p->rot_speed = P1_ROT_SPEED;
+       ft_bzero(p, sizeof(t_player));
+       i = 0;
+       while (i < map->width * map->height)
+       {
+           if (map->grid[i] != '\0' && ft_strchr(PLYR_DIR, map->grid[i]))
+               break ;
+           i++;
+       }
+       if (i >= map->width * map->height)
+       {
+               write(2, "Error: No player found in map\n", 30);
+               exit(1);
+       }
+       p->pos_x = (double)(i % map->width) + 0.5;
+       p->pos_y = (double)(i / map->width) + 0.5;
+       get_dir(p, map->grid[i]);
+       map->grid[i] = '0';
+       p->camp_x = -p->dir_y * (tan(fov / 2));
+       p->camp_y = p->dir_x * (tan(fov / 2));
+       p->speed = P1_SPEED;
+       p->rot_speed = P1_TURNSP;
+       p->cos_r = cos(p->rot_speed);
+       p->sin_r = sin(p->rot_speed);
+       p->cos_l = cos(-p->rot_speed);
+       p->sin_l = sin(-p->rot_speed);
+       p->fov = fov;
+       p->dir_mag = sqrt(p->dir_x * p->dir_x + p->dir_y * p->dir_y);
+       p->radius = P1_R;
+       p->fwd_speed = P1_SPEED;
+       p->rot_speed = P1_ROT_SPEED;
 }
 
-// TODO: Proper cleanup
-int	alloc_sprites(t_win *win)
+int    alloc_sprites(t_win *win)
 {
-	int	i;
+       int     i;
 
-	win->sprites = ft_calloc(N_SPRITES, sizeof(t_sprite));
-	if (!win->sprites)
-	{
-		free(win->z_buffer);
-		write(2, "Error: malloc\n", 8);
-		exit(EXIT_FAILURE);
-	}
-	i = 0;
-	while (i < N_SPRITES)
-	{
-		win->sprites[i].tex = ft_calloc(N_FIRES, sizeof(t_img));
-		if (!win->sprites[i].tex)
-		{
-			exit(EXIT_FAILURE);
-		}
-		i++;
-	}
-	win->sprite_dist = ft_calloc(N_SPRITES, sizeof(double));
-	if (!win->sprite_dist)
-	{
-		free(win->z_buffer);
-		free(win->sprites);
-		write(2, "Error: malloc\n", 8);
-		exit(EXIT_FAILURE);
-	}
-	win->sprite_order = ft_calloc(N_SPRITES, sizeof(int));
-	if (!win->sprite_order)
-	{
-		free(win->z_buffer);
-		free(win->sprites);
-		free(win->sprite_dist);
-		write(2, "Error: malloc\n", 8);
-		exit(EXIT_FAILURE);
-	}
+       win->sprites = ft_calloc(N_SPRITES, sizeof(t_sprite));
+       if (!win->sprites)
+       {
+               free(win->z_buffer);
+               write(2, "Error: malloc\n", 8);
+               exit(EXIT_FAILURE);
+       }
+       i = 0;
+       while (i < N_SPRITES)
+       {
+               win->sprites[i].tex = ft_calloc(N_FIRES, sizeof(t_img));
+               if (!win->sprites[i].tex)
+               {
+                       exit(EXIT_FAILURE);
+               }
+               i++;
+       }
+       win->sprite_dist = ft_calloc(N_SPRITES, sizeof(double));
+       if (!win->sprite_dist)
+       {
+               free(win->z_buffer);
+               free(win->sprites);
+               write(2, "Error: malloc\n", 8);
+               exit(EXIT_FAILURE);
+       }
+       win->sprite_order = ft_calloc(N_SPRITES, sizeof(int));
+       if (!win->sprite_order)
+       {
+               free(win->z_buffer);
+               free(win->sprites);
+               free(win->sprite_dist);
+               write(2, "Error: malloc\n", 8);
+               exit(EXIT_FAILURE);
+       }
+       return (0);
+}
+
+void   setup_mlx(t_win *win, t_img *img)
+{
+       win->exit_status = EXIT_SUCCESS;
+       win->mlxptr = mlx_init();
+       if (!win->mlxptr)
+       {
+               write(2, "Error: mlx_init\n", 16);
+               exit(1);
+       }
+       img->line_len = 0;
+       img->bpp = 4;
+       win->winptr = mlx_new_window(win->mlxptr, WIDTH, HEIGHT, "cub3d");
+       if (!win->winptr)
+       {
+               write(2, "Error: mlx_new_window\n", 22);
+               exit(1);
+       }
+       mlx_hook(win->winptr, KeyPress, KeyPressMask, key_press, win);
+       mlx_hook(win->winptr, KeyRelease, KeyReleaseMask, key_release, win);
+       mlx_hook(win->winptr, DestroyNotify, 0, close_win, win);
+       mlx_hook(win->winptr, MotionNotify, PointerMotionMask, mouse_hook, win);
+       img->img = mlx_new_image(win->mlxptr, WIDTH, HEIGHT);
+       if (!img->img)
+       {
+               win->exit_status = EXIT_FAILURE;
+               write(2, "Error: mlx_new_image\n", 21);
+               close_win(win);
+       }
+       img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->line_len,
+                       &img->endian);
+       if (!img->addr)
+       {
+               win->exit_status = EXIT_FAILURE;
+               write(2, "Error: mlx_get_data_addr\n", 25);
+               close_win(win);
+       }
+       img->width = WIDTH;
+       img->height = HEIGHT;
+       mlx_mouse_move(win->mlxptr, win->winptr, WIDTH / 2, HEIGHT / 2);
+       mlx_mouse_hide(win->mlxptr, win->winptr);
+}
+
+int	init_textures(t_win *win)
+{
+	if (load_texture(win, &win->textures[W1N], win->map->no_path)
+		|| load_texture(win, &win->textures[W1S], win->map->so_path)
+		|| load_texture(win, &win->textures[W1W], win->map->we_path)
+		|| load_texture(win, &win->textures[W1E], win->map->ea_path))
+		return (1);
 	return (0);
-}
-
-void	setup_mlx(t_win *win, t_img *img)
-{
-	win->exit_status = EXIT_SUCCESS;
-	win->mlxptr = mlx_init();
-	if (!win->mlxptr)
-		exit(1);
-	img->line_len = 0;
-	img->bpp = 4;
-	win->winptr = mlx_new_window(win->mlxptr, WIDTH, HEIGHT, "cub3d");
-	if (!win->winptr)
-	{
-		free(win->mlxptr);
-		write(2, "Error: mlx_new_window\n", 23);
-		exit(1);
-	}
-	mlx_hook(win->winptr, KeyPress, KeyPressMask, key_press, win);
-	mlx_hook(win->winptr, KeyRelease, KeyReleaseMask, key_release, win);
-	mlx_hook(win->winptr, DestroyNotify, 0, close_win, win);
-	mlx_hook(win->winptr, MotionNotify, PointerMotionMask, mouse_hook, win);
-	img->img = mlx_new_image(win->mlxptr, WIDTH, HEIGHT);
-	if (!img->img)
-	{
-		win->exit_status = EXIT_FAILURE;
-		write(2, "Error: mlx_new_image\n", 22);
-		close_win(win);
-	}
-	img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->line_len,
-			&img->endian);
-	if (!img->addr)
-	{
-		win->exit_status = EXIT_FAILURE;
-		write(2, "Error: mlx_get_data_addr\n", 26);
-		close_win(win);
-	}
-	img->width = WIDTH;
-	img->height = HEIGHT;
-	mlx_mouse_move(win->mlxptr, win->winptr, WIDTH / 2, HEIGHT / 2);
-	mlx_mouse_hide(win->mlxptr, win->winptr);
 }
