b9f313422e807b7e0d73c64676d60794304d42da
Álvaro Morenes Muro <alvaromorenesmuro@MacBook-Pro-de-Alvaro.local>
2026-04-07 20:05:45 +0200

    cambiosdeultimahora

--- src/hooks.c
+++ src/hooks.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
+/*   By: almorene <almorene@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 17:10:00 by rafael            #+#    #+#             */
-/*   Updated: 2026/03/06 17:10:00 by rafael           ###   ########.fr       */
+/*   Updated: 2026/04/07 19:41:53 by almorene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
+/*#include "../includes/cub3d.h"
 
 int	close_win(const t_win *win)
 {
@@ -28,6 +28,49 @@ int	close_win(const t_win *win)
 		free(win->map->grid);
 		free(win->map);
 	}
+	exit(win->exit_status);
+}
+
+int	game_loop(const t_win *win)
+{
+	int		render;
+
+	render = handle_input(win);
+	if (render)
+	{
+		render_frame(win);
+		mlx_put_image_to_window(win->mlxptr, win->winptr, win->img->img, 0, 0);
+	}
+	return (0);
+}*/
+
+#include "../includes/cub3d.h"
+
+int	close_win(const t_win *win)
+{
+	// Destruye la ventana y la imagen
+	if (win->winptr)
+		mlx_destroy_window(win->mlxptr, win->winptr);
+	if (win->img && win->img->img)
+		mlx_destroy_image(win->mlxptr, win->img->img);
+
+	// Diferencia Linux / Mac
+#ifdef __linux__
+	if (win->mlxptr)
+	{
+		mlx_destroy_display(win->mlxptr);
+		free(win->mlxptr);
+	}
+#endif
+	// Mac libera solo lo que haga falta (MLX lo gestiona internamente)
+
+	// Libera mapa
+	if (win->map)
+	{
+		free(win->map->grid);
+		free(win->map);
+	}
+
 	exit(win->exit_status);
 }
 
