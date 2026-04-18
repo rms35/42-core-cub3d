f4b3a563b4b5e305de22c9868ea6bde9bf11192a
rms35 <martinsoeder35@gmail.com>
2026-04-19 01:13:17 +0200

    feat(bonus): proper rendering of the door implemented

--- bonus/engine/render_door.c
+++ bonus/engine/render_door.c
@@ -1,53 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render.c                                           :+:      :+:    :+:   */
+/*   render_door.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 21:56:33 by rafael            #+#    #+#             */
-/*   Updated: 2026/03/06 18:25:00 by rafael           ###   ########.fr       */
+/*   Updated: 2026/04/19 14:00:00 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
 
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ray.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/06 17:15:00 by rafael            #+#    #+#             */
-/*   Updated: 2026/03/06 17:15:00 by rafael           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../../includes/cub3d_bonus.h"
-
-static void init_for_dda(const t_win *win, t_ray *ray)
-{
-	ray->delta_dist_x = fabs(1 / ray->dir_x);
-	ray->delta_dist_y = fabs(1 / ray->dir_y);
-	ray->hit = 0;
-	ray->step_x = 1 - 2 * (ray->dir_x < 0);
-	if (ray->dir_x < 0)
-		ray->side_dist_x = (win->player->pos_x - ray->map_x)
-							* ray->delta_dist_x;
-	else
-		ray->side_dist_x = (ray->map_x + 1.0 - win->player->pos_x)
-							* ray->delta_dist_x;
-	ray->step_y = 1 - 2 * (ray->dir_y < 0);
-	if (ray->dir_y < 0)
-		ray->side_dist_y = (win->player->pos_y - ray->map_y)
-							* ray->delta_dist_y;
-	else
-		ray->side_dist_y = (ray->map_y + 1.0 - win->player->pos_y)
-							* ray->delta_dist_y;
-}
-
-static void	get_wall_x(const t_win *win, t_ray *ray)
+static void	get_door_wall_x(const t_win *win, t_ray *ray)
 {
 	if (ray->side == 0)
 		ray->wall_x = win->player->pos_y + ray->perp_dist * ray->dir_y;
@@ -60,7 +25,7 @@ static void	get_wall_x(const t_win *win, t_ray *ray)
 		ray->wall_x = 1.0 - ray->wall_x;
 }
 
-int	door_dda(const t_win *win, t_ray *ray)
+static int	door_dda(const t_win *win, t_ray *ray, const t_sprite *door)
 {
 	while (ray->hit == 0)
 	{
@@ -78,25 +43,32 @@ int	door_dda(const t_win *win, t_ray *ray)
 		}
 		if (win->map->grid[ray->map_y * win->map->width + ray->map_x] == '1')
 			return (1);
-		if (win->map->grid[ray->map_y * win->map->width + ray->map_x] == 'D')
-			ray->hit = 1;
+		if (win->map->grid[ray->map_y * win->map->width + ray->map_x] == 'D'
+			&& ray->map_x == (int)door->x && ray->map_y == (int)door->y)
+		{
+			if (ray->side == 0)
+			{
+				ray->perp_dist = ray->side_dist_x - 0.5 * ray->delta_dist_x;
+				if (floor(win->player->pos_y + ray->perp_dist * ray->dir_y) == ray->map_y)
+					ray->hit = 1;
+			}
+			else
+			{
+				ray->perp_dist = ray->side_dist_y - 0.5 * ray->delta_dist_y;
+				if (floor(win->player->pos_x + ray->perp_dist * ray->dir_x) == ray->map_x)
+					ray->hit = 1;
+			}
+		}
 	}
-	if (ray->side == 0)
-		ray->perp_dist = (ray->map_x - win->player->pos_x + (1 -
-			ray->step_x) / 2) / ray->dir_x;
-	else
-		ray->perp_dist = (ray->map_y - win->player->pos_y + (1 -
-			ray->step_y) / 2) / ray->dir_y;
-	get_wall_x(win, ray);
+	get_door_wall_x(win, ray);
 	return (0);
 }
 
-
-static void	draw_door(const t_win *win, t_ray *ray, const int int_x, int y)
+static void	draw_door_pixel(const t_win *win, t_ray *ray, int x_offset, int y)
 {
-	int		tex_y;
-	int		tex_x;
-	int		color;
+	int				tex_y;
+	int				tex_x;
+	unsigned int	color;
 
 	tex_y = (int)ray->tex_pos % ray->tex->height;
 	if (tex_y < 0)
@@ -109,22 +81,21 @@ static void	draw_door(const t_win *win, t_ray *ray, const int int_x, int y)
 	ray->tex_pos += ray->tex_step;
 	color = *(unsigned int *)(ray->tex->addr + (tex_y * ray->tex->line_len
 				+ tex_x * (ray->tex->bpp / 8)));
-	*(unsigned int *)(win->img->addr + (y * win->img->line_len + int_x)) =
-		color;
+	if ((color & 0x00FFFFFF) != 0)
+		*(unsigned int *)(win->img->addr + (y * win->img->line_len + x_offset)) =
+			color;
 }
 
-
-
-static void	draw_line(const t_win *win, t_ray *ray, const int x)
+static void	draw_door_column(const t_win *win, t_ray *ray, int x)
 {
 	int	y;
-	int	offset;
+	int	x_offset;
 
 	y = ray->draw_start;
-	offset = x * (win->img->bpp / 8);
+	x_offset = x * (win->img->bpp / 8);
 	while (y < ray->draw_end)
 	{
-		draw_door(win, ray, offset, y);
+		draw_door_pixel(win, ray, x_offset, y);
 		y++;
 	}
 }
@@ -139,9 +110,8 @@ void	render_door(const t_win *win, const t_sprite *door)
 	ray.tex = win->door;
 	while (x < WIDTH)
 	{
-		init_for_dda(win, &ray);
 		init_ray(win, &ray, x);
-		if (door_dda(win, &ray))
+		if (door_dda(win, &ray, door))
 		{
 			x++;
 			continue ;
@@ -150,17 +120,17 @@ void	render_door(const t_win *win, const t_sprite *door)
 			ray.perp_dist = 0.1;
 		win->z_buffer[x] = ray.perp_dist;
 		center_ofs = (HEIGHT / 2) + win->player->pitch;
-		ray.line_height = (int)((double)HEIGHT / ray.perp_dist);
+		ray.line_height = (int)((double)HEIGHT / ray.perp_dist) / 2;
 		ray.draw_start = center_ofs - (ray.line_height / 2);
 		if (ray.draw_start < 0)
 			ray.draw_start = 0;
-		ray.draw_end = center_ofs + (ray.line_height/ 2);
+		ray.draw_end = center_ofs + (ray.line_height / 2);
 		if (ray.draw_end >= HEIGHT)
 			ray.draw_end = HEIGHT - 1;
 		ray.tex_step = 1.0 * door->tex->height / ray.line_height;
-		ray.tex_pos = (ray.draw_start - center_ofs + ray.line_height / 2) *
-			ray.tex_step;
-		draw_line(win, &ray, x);
+		ray.tex_pos = (ray.draw_start - center_ofs + ray.line_height / 2)
+			* ray.tex_step;
+		draw_door_column(win, &ray, x);
 		x++;
 	}
 }
