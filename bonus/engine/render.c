ccea56d56a9321dfa0e60e788ceb039b4bd9596a
rms35 <martinsoeder35@gmail.com>
2026-04-20 23:51:18 +0200

    refactor(bonus): optimize wall rendering and separate background drawing

--- bonus/engine/render.c
+++ bonus/engine/render.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 21:56:33 by rafael            #+#    #+#             */
-/*   Updated: 2026/04/19 12:00:00 by rafael           ###   ########.fr       */
+/*   Updated: 2026/04/20 12:00:00 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,53 +26,55 @@ static void	draw_wall(t_ray *ray, unsigned int *dest, int tex_x)
 	*dest = color;
 }
 
-static void	draw_line(t_win *win, t_ray *ray, int x)
+static void	draw_background(const t_win *win, int x, int start, int end)
+{
+	int				y;
+	int				line_stride;
+	unsigned int	*dest;
+
+	line_stride = win->img->line_len / 4;
+	dest = (unsigned int *)win->img->addr + x;
+	y = 0;
+	while (y < start)
+	{
+		dest[y * line_stride] = win->map->ceil_color;
+		y++;
+	}
+	y = end;
+	while (y < HEIGHT)
+	{
+		dest[y * line_stride] = win->map->floor_color;
+		y++;
+	}
+}
+
+static void	draw_line(const t_win *win, t_ray *ray, int x)
 {
 	int				y;
 	int				tex_x;
-	unsigned int	*dest;
 	int				line_stride;
+	unsigned int	*dest;
 
-	y = 0;
 	line_stride = win->img->line_len / 4;
+	draw_background(win, x, ray->draw_start, ray->draw_end);
 	dest = (unsigned int *)win->img->addr + x;
 	tex_x = (int)(ray->wall_x * (double)ray->tex->width);
 	if (tex_x >= ray->tex->width)
 		tex_x = ray->tex->width - 1;
 	if (tex_x < 0)
 		tex_x = 0;
-	while (y < ray->draw_start)
-	{
-		*dest = win->map->ceil_color;
-		dest += line_stride;
-		y++;
-	}
+	y = ray->draw_start;
 	while (y < ray->draw_end)
 	{
-		draw_wall(ray, dest, tex_x);
-		dest += line_stride;
+		draw_wall(ray, &dest[y * line_stride], tex_x);
 		y++;
 	}
-	while (y < HEIGHT)
-	{
-		*dest = win->map->floor_color;
-		dest += line_stride;
-		y++;
-	}
-}
-
-static void	get_wall(t_ray *ray, t_img *tex, t_map *map)
-{
-	int	pos;
-	char c;
-
-	pos = ray->map_x + (ray->map_y * map->width);
-	c = map->grid[pos];
-	ray->tex = &tex[c - 49];
 }
 
 static void	calculate_ray_bounds(t_win *win, t_ray *ray, int center_ofs)
 {
+	int	pos;
+
 	if (ray->perp_dist < 0.1)
 		ray->perp_dist = 0.1;
 	ray->line_height = (int)((double)HEIGHT / ray->perp_dist) / 2;
@@ -82,7 +84,8 @@ static void	calculate_ray_bounds(t_win *win, t_ray *ray, int center_ofs)
 	ray->draw_end = center_ofs + (ray->line_height / 2);
 	if (ray->draw_end >= HEIGHT)
 		ray->draw_end = HEIGHT - 1;
-	get_wall(ray, win->textures, win->map);
+	pos = ray->map_x + (ray->map_y * win->map->width);
+	ray->tex = &win->textures[win->map->grid[pos] - 49];
 	ray->tex_step = 1.0 * ray->tex->height / ray->line_height;
 	ray->tex_pos = (ray->draw_start - center_ofs + ray->line_height / 2)
 		* ray->tex_step;
